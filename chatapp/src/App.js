ee91c247f7209f98013eea793102d5c58746abc2
Saswat1998 <papalipratap@gmail.com>
2021-01-03 03:15:39 +0530

    Home, login and chat page added

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Switch, Route, BrowserRouter } from 'react-router-dom';
+import Home from './home';
+import Chat from './chat';
+import Login from './login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Home Page</h1>
+    <div style={{backgroundImage:"URL('./collaborative.jpg')", position:"absolute", top:"0", bottom:"0", width:"100%"}}>
+      <BrowserRouter>
+        <div className="App">
+          <h1 style={{backgroundColor:"blue",color:"white"}}>A collaborative Study Environment</h1>       
+          <nav class="nav justify-content-center" style={{backgroundColor:"green", height:"2em", marginTop:10}}>
+            <Link class="nav-link" style={{color:'white'}} to="/home">Home</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+            <Link class="nav-link" style={{color:'white'}} to="/login">Login</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+            <Link class="nav-link" style={{color:'white'}} to="/chat">Chat</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          </nav>
+        </div>
+        <switch>
+          <Route exact path="/home" component={Home}/>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/chat" component={Chat}/>
+          <Route exact path="/login" component={Login}/>
+        </switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ chatapp/src/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Home extends Component{
+    render(){
+        return(
+            <div class="container-fluid">
+                <h1 style={{textAlign:'center'}}>Home page</h1>
+            </div>
+        )
+    }
+}
+export default Home;
